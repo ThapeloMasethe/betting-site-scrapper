70270a1f309af25e8243374da7fa2b39963256b1
Thapelo Masethe <Thapelo.Masethe@fnb.co.za>
2022-04-19 00:01:34 +0200

    Initial commit

---
+++ src/main/java/za/co/thapelomasethe/bettingsitescrapper/BettingSiteScrapperApplication.java
@@ -0,0 +1,13 @@
+package za.co.thapelomasethe.bettingsitescrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BettingSiteScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BettingSiteScrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/thapelomasethe/bettingsitescrapper/BettingSiteScrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.thapelomasethe.bettingsitescrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BettingSiteScrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
